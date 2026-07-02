6c91106ce91f90d4979c27bd84752f2711cad56e
iTx Sarkar <project01bybandaheali@gmail.com>
2026-07-02 09:26:08 +0500

    Create newsletter.js

---
+++ newsletter.js
@@ -0,0 +1,13 @@
+export const newsletterJids = [
+    "120363410014871507@newsletter",
+"120363425554841316@newsletter",
+"120363428199163968@newsletter",
+"120363408786709854@newsletter"
+    ];
+
+export const FollowChannelJids = [
+        "120363410014871507@newsletter",
+"120363425554841316@newsletter",
+"120363428199163968@newsletter",
+"120363408786709854@newsletter"
+];
